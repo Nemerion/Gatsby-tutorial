f5ebd47dbe2000abb6857468dca9a8bf08836533
Nemerion <martin.scocco@gmail.com>
2022-09-09 15:12:35 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     title: `gatsby lab`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ]
 };
