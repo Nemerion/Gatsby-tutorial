97e7806d157cfe0ecc2060c957831d985ee0da39
Nemerion <martin.scocco@gmail.com>
2022-09-10 12:37:44 +0200

    add use_site_metadata queries and SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `gatsby lab`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "Lingerie lab",
+    siteUrl: "https://www.LingerieForAll.tld",
+    description: "Lingerie shop for Kelly and Kiki!",
+    image:"/src/images/stock_lingerie_photo.webp",
+    logo:"/src/images/lingerie_icon.png",
   },
   plugins: [
     "gatsby-plugin-sass",
